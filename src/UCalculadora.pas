77d8173bb00fc4683f218991afa4cde909ea1394
Evandro da Silva Amparo <evandro.amparo@gmail.com>
2020-04-04 23:49:54 -0300

    soma

--- src/UCalculadora.pas
+++ src/UCalculadora.pas
@@ -5,10 +5,18 @@ interface
 type
    TCalculadora = class
    public
+      function Somar(x, y: double): double;
    end;
 
 implementation
 
 { TCalculadora }
+
+{ TCalculadora }
+
+function TCalculadora.Somar(x, y: double): double;
+begin
+
+end;
 
 end.
