685c670b884756af602917c870be9eee39166403
Evandro da Silva Amparo <evandro.amparo@gmail.com>
2020-04-21 22:28:27 -0300

    build & tests separados

--- test.cmd
+++ test.cmd
@@ -1,9 +1,5 @@
 @echo off
 
-msbuild /target:Build /property:config=Release /verbosity:quiet /nologo tests\CalculadoraTests.dproj
-
-if %ERRORLEVEL% neq 0 exit /b 1
-
 cd tests\bin\
 
 powershell ".\CalculadoraTests.exe | tee tests.txt"
