b514ec5d680990b033cde445be603423b5f1ee9e
Evandro da Silva Amparo <1426462+evandroamparo@users.noreply.github.com>
2020-06-18 21:08:44 -0300

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 @echo off
 
-msbuild /target:Build /property:config=Release /verbosity:quiet /nologo tests\CalculadoraTests.dproj
+msbuild /target:Build /property:config=Release /nologo tests\CalculadoraTests.dproj
 
 if %ERRORLEVEL% neq 0 exit /b 1
